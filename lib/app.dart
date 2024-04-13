3ac6d76c1613acc833213d7700796cadc6073106
RitsuMurakami <beinosuke01@gmail.com>
2024-04-14 08:55:10 +0900

    fin: ligtmode test

--- lib/app.dart
+++ lib/app.dart
@@ -10,33 +10,33 @@ class TypistApp extends StatelessWidget {
     return MaterialApp(
       title: 'Typist',
       debugShowCheckedModeBanner: false,
-      //theme: ThemeData(
-      //  colorScheme: const ColorScheme.light(
-      //    primary: Colors.black, // 主要な色
-      //    secondary: Colors.grey, // セカンダリ色
-      //    surface: Colors.white, // 表面色
-      //    background: Colors.white, // 背景色
-      //    error: Colors.red, // エラー色
-      //    onPrimary: Colors.white, // 主要な色の上でのテキスト色
-      //    onSecondary: Colors.black,
-      //    onSurface: Colors.black,
-      //    onBackground: Colors.black,
-      //    onError: Colors.red, // エラー色の上でのテキスト色
-      //    brightness: Brightness.light, // 明るさ
-      //  ),
-      //  useMaterial3: true,
-      //),
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+          primary: Colors.black, // 主要な色
+          secondary: Colors.grey, // セカンダリ色
+          surface: Colors.white, // 表面色
+          background: Colors.white, // 背景色
+          error: Colors.red, // エラー色
+          onPrimary: Colors.white, // 主要な色の上でのテキスト色
+          onSecondary: Colors.black,
+          onSurface: Colors.black,
+          onBackground: Colors.black,
+          onError: Colors.red, // エラー色の上でのテキスト色
+          brightness: Brightness.light, // 明るさ
+        ),
+        useMaterial3: true,
+      ),
       darkTheme: ThemeData(
         colorScheme: ColorScheme.dark(
           primary: Colors.grey.shade800, // 主要な色
           secondary: Colors.white, // セカンダリ色
-          surface: Color(0xFF363636), // 表面色
+          surface: const Color(0xFF363636), // 表面色
           error: Colors.red, // エラー色
           onPrimary: Colors.white, // 主要な色の上でのテキスト色
           onSecondary: Colors.white,
           onSurface: Colors.white,
           onBackground: Colors.white,
-          background: Color(0xff1E1E1E),
+          background: const Color(0xff1E1E1E),
           onError: Colors.red, // エラー色の上でのテキスト色
         ),
         useMaterial3: true,
---
+++ lib/pages/settings.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Settings extends StatefulWidget {
+  const Settings({super.key});
+
+  @override
+  State<Settings> createState() => _Settings();
+}
+
+class _Settings extends State<Settings> {
+  @override
+  Widget build(BuildContext) {
+    return Scaffold();
+  }
+}
