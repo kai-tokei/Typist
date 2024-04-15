f2877fc664d5375f8cbb984824e296d8501ea8d8
RitsuMurakami <beinosuke01@gmail.com>
2024-04-15 18:20:55 +0900

    add: message singletono

--- lib/consts/messages.dart
+++ lib/consts/messages.dart
@@ -3,4 +3,18 @@ import 'package:typist/consts/message_event.dart';
 class Messages {
   Messages._();
   static final instance = Messages._();
+
+  List<MessageEvent> events = [];
+
+  void add(MessageEvent event) {
+    events.add(event);
+  }
+
+  void remove(MessageEvent event) {
+    events.remove(event);
+  }
+
+  List<MessageEvent> getAll() {
+    return events;
+  }
 }
